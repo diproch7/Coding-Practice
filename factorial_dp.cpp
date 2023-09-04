69d328ea9f7da13d801bdff7a0da5bfaf6aae30b
diproch7 <34616184+diproch7@users.noreply.github.com>
2023-09-04 22:40:11 +0530

    Create factorial_dp.cpp
    Factorial of a Big Number with DP

---
+++ factorial_dp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	cin >> t;    //Reading input from STDIN
+	long long int mod = 1000000007;
+	long long int factorial[100001];
+	factorial[0]=1;factorial[1]=1;
+	for(int i=2;i<=100000;i++)
+	{
+		factorial[i]=(i*factorial[i-1])%mod;
+	}
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		cout<<factorial[n]<<endl;
+
+	}	
+}
