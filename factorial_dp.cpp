42e7d1c0b3b022dc0af2bd322a74853969162f59
diproch7 <34616184+diproch7@users.noreply.github.com>
2023-09-04 22:40:58 +0530

    Update factorial_dp.cpp

--- factorial_dp.cpp
+++ factorial_dp.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main() {
 	int t;
 	cin >> t;    //Reading input from STDIN
-	long long int mod = 1000000007;
+	long long int mod = 1000000007; //Mod 10^9+7
 	long long int factorial[100001];
 	factorial[0]=1;factorial[1]=1;
 	for(int i=2;i<=100000;i++)
 	{
-		factorial[i]=(i*factorial[i-1])%mod;
+		factorial[i]=(i*factorial[i-1])%mod; // Stores factorial
 	}
 	while(t--)
 	{
